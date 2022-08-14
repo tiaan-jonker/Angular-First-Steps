b4b121732639be8bb6697c44e8eb95336077c21f
tiaan-jonker <tiaanj06@gmail.com>
2022-08-15 11:02:39 +1200

    Button get password method added

--- pw-gen/src/app/app.component.ts
+++ pw-gen/src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pw-gen';
+  password = '';
+
+  onButtonClick() {
+    this.password = 'My password';
+    console.log('Button Clicked');
+  }
+
+  getPassword() {
+    return this.password;
+  }
 }
